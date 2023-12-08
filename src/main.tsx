a6cc059cac0e235bd84bcac190ccd7be5a93008e
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-12-08 08:02:08 -0300

    feat: Configura Context api

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import { router } from "./App.tsx";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 
+import CartProvider from "./contexts/CartContext.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <CartProvider>
     <RouterProvider router={router} />
+    </CartProvider>
   </React.StrictMode>
 );
